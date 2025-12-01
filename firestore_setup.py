f87337e7bd7d9d32cc9ee696941d4f6587ba5537
dinesh-24 <dineshnanda.official@gmail.com>
2025-12-01 23:10:56 +0530

    file updated

--- firestore_setup.py
+++ firestore_setup.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
 
 if not firebase_admin._apps:
-    # Read the JSON string from secrets
-    service_account_info = json.loads(st.secrets["firebase_json"])
-    cred = credentials.Certificate(service_account_info)
+# Read the [firebase] section as a dict
+    firebase_config = dict(st.secrets["firebase"])
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 def get_db():
-    return get_db()
+    return db
