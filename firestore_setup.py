d4f0c64698266eb2fb0066729902ca31d4ca941d
dinesh-24 <dineshnanda.official@gmail.com>
2025-12-01 22:45:53 +0530

    pushing for streamlit

--- firestore_setup.py
+++ firestore_setup.py
@@ -1,10 +1,15 @@
-# firestore_setup.py
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase (only once)
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+# Read the [firebase] section from Streamlit secrets
+    firebase_config = dict(st.secrets["firebase"])
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
 
+# Initialize Firebase (only once)
 db = firestore.client()
+
+def get_db():
+    return db
